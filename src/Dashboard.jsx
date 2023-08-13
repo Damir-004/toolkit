1f11d9a650bb7f2e1d6936bdce0c137408728fd7
Damir <azizqalminip@Damir2004-git>
2023-08-13 22:36:53 +0500

    first commit

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,39 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
 import { Link } from "react-router-dom";
-import { fetchPosts } from './postsSlice';
 
 const Dashboard = () => {
-  const dispatch = useDispatch();
-  const posts = useSelector((state) => state.posts.posts);
-  const error = useSelector((state) => state.posts.error);
-  const loading = useSelector((state) => state.posts.loading);
-
-  const [showPosts, setShowPosts] = useState(false);
-
-  const handleClick = () => {
-    dispatch(fetchPosts());
-    setShowPosts(false);
-  };
-
-  useEffect(() => {
-    if (!loading) {
-      const timer = setTimeout(() => {
-        setShowPosts(true);
-      }, 700);
-
-      return () => clearTimeout(timer);
-    }
-  }, [loading]);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
-
+  
   return (
     <div>
         <div className='top_block'>
@@ -49,20 +18,7 @@ const Dashboard = () => {
         <div className='main container'>
             <h1>Dashboard</h1>
             <p>This is the dashboard</p>
-            <button onClick={handleClick} disabled={loading}>
-                {loading ? 'Loading...' : 'View Posts'}
-            </button>
-            {showPosts && (
-                <ul>
-                    {posts.map((post) => (
-                        <li key={post.id}>
-                            <h1>{post.name}</h1>
-                            <h3>{post.phone}</h3>
-                            <Link  to="/Next">View Post</Link>
-                        </li>
-                    ))}
-                </ul>
-            )}
+            <Link  to="/ViewPost">View Post</Link>
         </div>
     </div>
   );
